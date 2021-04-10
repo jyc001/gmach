17bbcd8cc0f8f9838483f4f43cbe2313e5d39ce3
jyc0001 <80881099@qq.com>
2021-04-10 19:27:07 +0800

    fix all final

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ import time
 import csv
 
 db = SqliteDatabase('self.db')
-
-
+db.connect()
+ll = []
+l2 = []
 
 
 class base(Model):
@@ -23,49 +24,35 @@ class base(Model):
         database = db
 
 
+for i in base.select():
+    ll.append(i.Title)
+    l2.append(i.Alternate_Title)
+
+
 def func(year, employer, name, title, annual_wages, source, predictedgender, nameclean):
+    global ll, l2
     if nameclean is None or nameclean == '':
         return year, employer, name, title, annual_wages, source, predictedgender, nameclean, '', 0, '', 0
-    ar = base.select().where(SQL(f"""
-    Title like "%{nameclean}%"
-    """))
-    if ar.exists():
-        ll = []
-        for i in ar:
-            ll.append(i.id)
-        _res = proc.extractBests(nameclean, ll, score_cutoff=80, limit=1)
-        if not _res:
-            res1 = ""
-            soc1 = 0
-        else:
-            res1 = _res[0][0]
-            soc1 = _res[0][1]
-    else:
+    _res = proc.extractBests(nameclean, ll, score_cutoff=80, limit=1)
+    if not _res:
         res1 = ""
         soc1 = 0
-    ar = base.select().where(SQL(f"""
-    Alternate_Title like "%{nameclean}%"
-    """))
-    if ar.exists():
-        l2 = []
-        for i in ar:
-            l2.append(i.id)
-        _res = proc.extractBests(nameclean, l2, score_cutoff=80, limit=1)
-        if not _res:
-            res2 = ""
-            soc2 = 0
-        else:
-            res2 = _res[0][0]
-            soc2 = _res[0][1]
     else:
+        res1 = _res[0][0]
+        soc1 = _res[0][1]
+    _res = proc.extractBests(nameclean, l2, score_cutoff=80, limit=1)
+    if not _res:
         res2 = ""
         soc2 = 0
+
+    else:
+        res2 = _res[0][0]
+        soc2 = _res[0][1]
     return year, employer, name, title, annual_wages, source, predictedgender, nameclean, res1, soc1, res2, soc2
 
 
 if __name__ == '__main__':
     print("starting...")
-    db.connect()
     # db.create_tables([base])
 
     # print(t_list)
@@ -79,13 +66,14 @@ if __name__ == '__main__':
     name_list = zip(df1['year'].to_list(), df1['employer'].to_list(), df1['name'].to_list(), df1['title'].to_list(),
                     df1['annual_wages'].to_list(), df1['source'].to_list(), df1['predictedgender'].to_list(),
                     df1['nameclean'].to_list())
+    # print(list(name_list))
     print("maching...")
     with Pool(15) as p:
         start = datetime.now()
         res = p.starmap_async(func, name_list)
         result = res.get()
         print('matching data cost %s s.' % str(datetime.now() - start))
-        print(result)
+        # print(result)
     print("outputting...")
     with open("out.csv", "w+") as file:
         f_csv = csv.writer(file)
@@ -93,4 +81,3 @@ if __name__ == '__main__':
             ["year", "employer", "name", "title", "annual_wages", "source", "predictedgender", "nameclean", "res1",
              "soc1", "res2", "soc2"])
         f_csv.writerows(result)
-
